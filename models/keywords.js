f8d8899148ea15b126c193e27dc7e5cea7076e5b
Destiny <dshillis1@gmail.com>
2019-04-26 16:53:28 -0600

    Fixed Search Navbar

--- models/keywords.js
+++ models/keywords.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const keywordSchema = new mongoose.Schema({
     word: { type: String, required: true, unique: true },
     meaning: { type: String, required: true },
+    count: String,
 });
 
 // keywordSchema.index({ word: 'text', meaning: 'text' });
